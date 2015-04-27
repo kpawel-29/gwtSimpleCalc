f6ecaf5980312dc8b7995d54020de748ef3af37a
kpawel-29 <pakadlubowski@gmail.com>
2015-04-28 00:50:29 +0200

    kalkulator 1 step done

--- src/gwt/myproject/client/Kalkulator.java
+++ src/gwt/myproject/client/Kalkulator.java
@@ -5,24 +5,64 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class Kalkulator implements EntryPoint {
 	
-	Button sendButton = new Button("Send");
-	Label nameField  = new Label();
-	int count = 0;
+	final Button sendButton = new Button("Send");
+	final Label wynikLabel  = new Label();
+	final TextBox tb1 = new TextBox();
+	final TextBox tb2 = new TextBox();
+	
+	final RadioButton rbPlus = new RadioButton("dzialanie", "+");
+	final RadioButton rbMinus = new RadioButton("dzialanie", "-");
+	final RadioButton rbRazy = new RadioButton("dzialanie", "*");
+	
+	
 	
 	public void onModuleLoad() {
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("nameFieldContainer").add(nameField);
+		RootPanel.get().add(tb1);
+		RootPanel.get().add(rbPlus);
+		rbPlus.setValue(true);
+		RootPanel.get().add(rbMinus);
+		RootPanel.get().add(rbRazy);
+		RootPanel.get().add(tb2);
+		
+		RootPanel.get().add(sendButton);
+		RootPanel.get().add(wynikLabel);
 		
 		sendButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				nameField.setText("klinkniecie nr: "+ ++count);
-				
+				if(rbPlus.getValue())
+					wynikLabel.setText(Oblicz(tb1.getText(),tb2.getText(),"+"));
+				if(rbMinus.getValue())
+					wynikLabel.setText(Oblicz(tb1.getText(),tb2.getText(),"-"));
+				if(rbRazy.getValue())
+					wynikLabel.setText(Oblicz(tb1.getText(),tb2.getText(),"*"));
+					
 			}
 		});
 
 	}
+	public String Oblicz(String s1, String s2, String dzialanie){
+		int wynik = 0;
+		int val1 = Integer.valueOf(s1);
+		int val2 = Integer.valueOf(s2);
+		switch (dzialanie){
+		case "+":
+			wynik = val1 + val2;
+			break;
+		case "-":
+			wynik = val1 - val2;
+			break;
+		case "*":
+			wynik = val1 * val2;
+			break;
+		}
+		return String.valueOf(wynik);
+	}
+	
 }
+
